4cbe3e7610b2baf2cf445d75c25a0ff2b35b95bc
siddh <siddh@192.168.1.40>
2024-11-21 07:35:50 +0530

    Hey My Commits here

--- services/discovery/src/main/java/com/alibou/discovery/DiscoveryApplication.java
+++ services/discovery/src/main/java/com/alibou/discovery/DiscoveryApplication.java
@@ -2,11 +2,11 @@ package com.alibou.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
